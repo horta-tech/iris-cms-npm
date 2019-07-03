bf6999b01de7bfc0ae9f64f56f0fb62ffe9a3aca
Rayan Castro <rayancdc@gmail.com>
2019-07-02 21:32:42 -0300

    Add Iris class constructor and fetch function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const Iris = (log) => {
-  console.log(log);
-};
+class Iris {
+  constructor(apiKey) {
+    this.apiKey = apiKey
+    this.indexUrl = 'https://iris.horta.dev/api/v1/posts?api_key=' + this.apiKey;
+  }
+
+  getPosts() {
+    fetch(this.indexUrl)
+    .then(response => response.json())
+    .then((myJson) => {
+      return myJson
+    })
+  }
+
+  postUrl(slug) {
+    return `https://iris.horta.dev/api/v1/posts/${slug}?api_key=` + this.apiKey;
+  }
+}
 
 export default Iris;
